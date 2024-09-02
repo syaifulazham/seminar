bfff2f1d7328c9c0bd58959968fafa3563944495
syaifulazham <120453871+syaifulazham@users.noreply.github.com>
2024-09-03 04:37:30 +0800

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const smtp_options = {
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT), // Convert to number
-    secure: false,
+    secure: process.env.SMTP_SECURE === "true", // Convert to boolean
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -25,7 +25,7 @@ if (!smtp_options.host || !smtp_options.port || !smtp_options.auth.user || !smtp
 
 export async function sendRegistrationEmail(email: string, participantId: number) {
   // Initialize the transporter
-  const transporter = nodemailer.createTransport(smtp_options_2);
+  const transporter = nodemailer.createTransport(smtp_options);
 
   // Send the email
   await transporter.sendMail({
@@ -37,7 +37,7 @@ export async function sendRegistrationEmail(email: string, participantId: number
 }
 
 export async function sendAcknowledgmentEmail(email: string) {
-  const transporter = nodemailer.createTransport(smtp_options_2);
+  const transporter = nodemailer.createTransport(smtp_options);
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
@@ -48,7 +48,7 @@ export async function sendAcknowledgmentEmail(email: string) {
 }
 
 export const sendApprovalEmail = async (email: string, name: string) => {
-  const transporter = nodemailer.createTransport(smtp_options_2);
+  const transporter = nodemailer.createTransport(smtp_options);
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
