bac5a8a3fc9c74f76bd637d7074f0674b934d91e
syaifulazham <120453871+syaifulazham@users.noreply.github.com>
2024-09-14 04:09:41 +0800

    add google map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,13 @@ import { FaEarlybirds } from "react-icons/fa6";
 import { AiFillSchedule, AiFillVideoCamera } from "react-icons/ai";
 import { LogoGroup } from "@/components/Logo";
 import { Navbar } from "@/components/Navbar";
+import GoogleMapView from "@/components/GoogleMapView";
+
 
 const Home = () => {
   return (
     <div className={styles.container}>
-    
+
       <Navbar active="home" />
       {/* Heading */}
       <h1 className={styles.heading}>KARNIVAL KESIHATAN PEKERJAAN </h1>
@@ -22,33 +24,66 @@ const Home = () => {
 
       <div className={styles.schedule_container}>
         <Image src={workers} alt="Logo 1" width={500} />
+
+        <div className="flex flex-col items-center justify-center mb-2 gap-10 p-4 w-full border-b border-white border-dashed" style={{ borderRadius: '35px' }}>
+          <h1 className={styles.headingSection}>TARIKH & LOKASI</h1>
+          
+        </div>
+        <div className="flex flex-col items-center justify-center mb-2 gap-10 p-4 w-full border-b border-white border-dashed md:flex-row" style={{ borderRadius: '35px' }}>
+          <div>
+            <h1 className={styles.heading2}>
+              BERTEMPAT DI 
+              <h1 className={styles.headingMadani2}>
+                SETIA CITY CONVENTION CENTER SHAH ALAM, SELANGOR
+              </h1>
+              PADA
+              <h1 className={styles.headingMadani2}>
+                26 HINGGA 27 OKTOBER 2024
+              </h1>
+            </h1>
+          </div>
+          <GoogleMapView />
+        </div>
+        
+
+        <div className="flex flex-col items-center justify-center mb-2 gap-10 p-4 w-full border-b border-white border-dashed" style={{ borderRadius: '35px' }}>
+          <h1 className={styles.headingSection}>TAJUK-TAJUK UTAMA PEMBENTANGAN
+            <h4 className='text-sm'>*Semakan jadual tentatif boleh disemak pada pautan <Link href="/tentatives">Tentatif</Link></h4>
+          </h1>
+        </div>
+
         <div className={styles.schedule_wrapper}>
           <div className={styles.schedule}>
             <div className={styles.grid_item_colspan_2_header}>26 OKTOBER 2024</div>
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Majlis Perasmian Garis Panduan Pentaksiran dan Pengurusan Risiko Psikososial Pekerjaan di Tempat Kerja 2024</div>
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Sinopsis Garis Panduan Pentaksiran dan Pengurusan Risiko Psikososial Pekerjaan di Tempat Kerja (PRisMA) 2024</div>
-            
+
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Cabaran Kesihatan Mental di Malaysia</div>
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Pentaksiran Risiko Psikososial Pekerjaan di Tempat Kerja (PRisMA)</div>
-            
+
             <div className={styles.grid_item_colspan_2}><AiFillSchedule className="text-2xl" />Pengenalan Berdasarkan Kajian Pembangunan Garis Panduan Pentaksiran dan Pengurusan Psikososial di Tempat Kerja</div>
-            
+
           </div>
 
           <div className={styles.schedule}>
             <div className={styles.grid_item_colspan_2_header}>27 OKTOBER 2024</div>
-            
+
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Pengurusan Risiko Psikososial Pekerjaan di Tempat Kerja</div>
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Aplikasi Garispanduan Pentaksiran dan Pengurusan Risiko Psikososial Pekerjaan di Tempat Kerja</div>
-            
+
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Psychological First Aid Programme</div>
             <div className={styles.item} ><AiFillSchedule className="text-2xl" />Perkongsian Pengurusan Kesihatan Mental di Industri</div>
-            
+
             <div className={styles.grid_item_colspan_2}><AiFillSchedule className="text-2xl" />Pengurusan Penyakit Psikososial di Tempat Kerja</div>
           </div>
         </div>
       </div>
 
+      <div className="flex flex-col items-center justify-center mb-2 gap-10 p-4 w-full border-b border-white border-dashed" style={{ borderRadius: '35px' }}>
+        <h1 className={styles.headingSection}>
+          PAKEJ SEMINAR
+        </h1>
+      </div>
       {/* Categories Section */}
       <div className={styles.categories}>
         {/* With HRDC Section */}
